110d1352b21a2a1575375b0c362c4077b44d1d65
Ignacio <root.betech.odoo@gmail.com>
2023-02-16 14:45:11 -0300

    arreglo commit

--- cliente/src/routers/RouterPrincipal.js
+++ cliente/src/routers/RouterPrincipal.js
@@ -1,27 +1,9 @@
 import {React} from "react"
-import {Routes, Route, BrowserRouter} from "react-router-dom"
-import { Cumpleaños } from "../componentes/Cumpleaños.jsx"
-import { Formulario } from "../componentes/Formulario.jsx"
-import { Header } from "../componentes/Header.jsx"
-import { Home } from "../componentes/Home.jsx"
-import { Lista } from "../componentes/Lista.jsx"
-import { Navbar } from "../componentes/Navbar.jsx"
 
 export const RouterPrincipal = () => {
   return (
        
-    <BrowserRouter>
-
-        <Header/>
-        <Navbar/>
-
-        <Routes>
-          <Route path='/' element={ <Home/> }/>
-          <Route path="/empleados" element={ <Lista/> }/>
-          <Route path='/cumpleaños' element={ <Cumpleaños/> } />
-          <Route path="/empleados/nuevo" element={ <Formulario/> }/>
-        </Routes>
-    </BrowserRouter>
+    <div></div>
   )
 }
 
